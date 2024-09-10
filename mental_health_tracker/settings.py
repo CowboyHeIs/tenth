866a0c732bdd86da1dd1e308ac73c587f7b758f6
Heinrich <heinrich.edric@cs.ui.ac.id>
2024-09-10 11:02:14 +0700

    mht3

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%r)co#%g16^-+^3@d(sd#a#s8^2&ptcy!(83q3u&k3-=eb+pv'
+SECRET_KEY = 'django-insecure-u%r)co#%g16^-+^3@d(sd#a#she8^2&ptcy!(83q3u&k3-=eb+pv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "http://pbp.cs.ui.ac.id/heinrich.edric/MHT2"
+    "http://heinrich-edric-mht3.pbp.cs.ui.ac.id/"
 ]
 
 
